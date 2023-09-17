b117cf66cecd2815014e5c4a4d0aa38127dfc891
John Ogama <jogama696@gmail.com>
2023-09-17 23:57:42 +0800

    feat: hero section wip �

---
+++ src/utils/colors/colors.ts
@@ -0,0 +1,15 @@
+interface Color {
+  background: string;
+  primary: string;
+  standardBg: string;
+  designBg: string;
+}
+
+const colors: Color = {
+  background: "#F1F1F1",
+  primary: "#2B2C29",
+  standardBg: "#EBEBEB",
+  designBg: "#DFDFDF",
+};
+
+export default colors;
