7b4a4588ef9318de288117ac992f91f656a3c186
John Ogama <jogama696@gmail.com>
2023-09-22 21:53:42 +0800

    fix error

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-    return config;
-  },
-};
+module.exports = nextConfig;
