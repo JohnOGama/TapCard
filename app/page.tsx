f466541f1295b13e504e556f91e8e33b22c27686
John Ogama <jogama696@gmail.com>
2023-09-23 00:37:20 +0800

    fix error in fs

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
         <CompanyDescription />
         <TrustedBrands />
         <ClientDesign />
+        <TrustedBrands />
       </main>
     </>
   );
--- pages/TrustedBrands.tsx
+++ pages/TrustedBrands.tsx
@@ -4,9 +4,18 @@ import path from "path";
 import Image from "next/image";
 import Title from "@/components/Title";
 
-export default async function TrustedBrands() {
+async function getStaticProps() {
   const imageDirectory = path.join(process.cwd(), "/public/Brands logo");
   const imageFilenames = await fs.readdir(imageDirectory);
+
+  return {
+    imageFilenames,
+  };
+}
+
+export default async function TrustedBrands() {
+  const { imageFilenames } = await getStaticProps();
+
   return (
     <div className="hidden lg:block w-full">
       <div className="max-w-[1200px] mx-auto py-16">
@@ -14,7 +23,7 @@ export default async function TrustedBrands() {
           Brands that trust tap Card
         </Title>
         <div className="grid grid-cols-6 gap-10">
-          {imageFilenames?.map((logo, index) => (
+          {imageFilenames.map((logo, index) => (
             <Image
               key={index}
               src={`/Brands logo/${logo}`}
