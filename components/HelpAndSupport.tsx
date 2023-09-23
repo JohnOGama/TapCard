a867c61e9b678c6547387dc21c0fa25074dde183
John Ogama <jogama696@gmail.com>
2023-09-24 05:14:44 +0800

    feat: done help and support feature

--- components/HelpAndSupport.tsx
+++ components/HelpAndSupport.tsx
@@ -1,53 +1,91 @@
 "use client";
 import React, { useState } from "react";
 import QuestionMark from "./icons/QuestionMark";
-import Chat from "./icons/Chat";
 import { AnimatePresence, motion } from "framer-motion";
+
+import Book from "@/components/icons/Book";
+import Circle from "@/components/icons/Circle";
+import DualChat from "@/components/icons/DualChat";
+import Chat from "@/components/icons/Chat";
+import Headphone from "@/components/icons/Headphone";
+import Star from "@/components/icons/Star";
+import Times from "./icons/Times";
+import Link from "next/link";
+
+const HelpAndSupportItems = [
+  {
+    icon: <Book width={"30"} height="30" />,
+    title: "Talk to sales",
+    description: "Learn more about our paid plans and enterprise options.",
+  },
+  {
+    icon: <Circle width="30" height="30" />,
+    title: "Help & Knowledge",
+    description: "Find help and video guides on all Builder features.",
+  },
+  {
+    icon: <DualChat width="30" height="30" />,
+    title: "Builder Community Forum",
+    description:
+      "Get technical assistance and support from the Builder community.",
+  },
+  {
+    icon: <Chat width="30" height="30" />,
+    title: "Contact Support",
+    description: "Submit a ticket to get help from our dedicated Support team.",
+  },
+  {
+    icon: <Headphone width="30" height="30" />,
+    title: "Get expert help",
+    description:
+      "Submit a project request to connect with an Expert from our partner ecosystem.",
+  },
+  {
+    icon: <Star width="30" height="30" />,
+    title: "Feature Suggestion",
+    description:
+      "Add to our feature wishlist and upvote your favorite requests.",
+  },
+];
 
 export default function HelpAndSupport() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <div className="fixed right-5 bottom-5 bg-secondary w-[50px] h-[50px] rounded-full cursor-pointer flex justify-center items-center">
-      <QuestionMark
-        color="white"
-        width="30"
-        height="30"
-        onClick={() => setIsOpen(!isOpen)}
-      />
+    <div
+      className="fixed z-10 right-5 bottom-5 bg-secondary w-[50px] h-[50px] rounded-full cursor-pointer flex justify-center items-center "
+      onClick={() => setIsOpen(!isOpen)}
+    >
       {isOpen ? (
-        <AnimatePresence>
-          <motion.div
-            initial={{ opacity: 0, y: 10 }}
-            animate={{
-              opacity: 1,
-              y: 0,
-              transition: { type: "spring", duration: 0.2, stiffness: 200 },
-            }}
-            exit={{ opacity: 0, y: 0, transition: { duration: 0.2 } }}
-            className="fixed bottom-[75px] right-10 bg-secondary flex flex-col  max-w-[300px]  rounded-md"
-          >
-            <div className="flex gap-5 hover:bg-primary/30 px-4 py-4">
-              <Chat width="40" height="40" />
-              <div className="text-primary capitalize">
-                <h1 className="">talk to sales</h1>
-                <p className="text-[14px]">
-                  learn more about our paid plans and enterprise options.
-                </p>
-              </div>
-            </div>
-            <div className="flex gap-5 hover:bg-primary/30 px-4  py-4">
-              <Chat width="40" height="40" />
-              <div className="text-primary capitalize">
-                <h1>talk to sales</h1>
-                <p className="text-[14px]">
-                  learn more about our paid plans and enterprise options.
-                </p>
-              </div>
-            </div>
-          </motion.div>
-        </AnimatePresence>
+        <>
+          <Times color="white" width="15" height="15" />
+          <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 0, y: 10 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { type: "spring", duration: 0.2, stiffness: 200 },
+              }}
+              exit={{ opacity: 0, y: 0, transition: { duration: 0.2 } }}
+              className="fixed border-2 border-primary/30 bottom-[75px] right-10 bg-secondary flex flex-col  max-w-[300px]  rounded-md "
+            >
+              {HelpAndSupportItems.map((item) => (
+                <Link
+                  href="/under-construction"
+                  className="flex gap-5 hover:bg-primary/30 px-4 py-4 duration-200"
+                >
+                  {item.icon}
+                  <div className="text-primary capitalize">
+                    <h1 className="">{item.title}</h1>
+                    <p className="text-[14px]">{item.description}</p>
+                  </div>
+                </Link>
+              ))}
+            </motion.div>
+          </AnimatePresence>
+        </>
       ) : (
-        ""
+        <QuestionMark color="white" width="30" height="30" />
       )}
     </div>
   );
--- data/StaticData.ts
+++ data/StaticData.ts
@@ -177,3 +177,4 @@ export const FooterContent = [
 
 
 
+
